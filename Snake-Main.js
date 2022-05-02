1b46ae7c8bc029c12a29ed8d178ac58a5c99c838
Rezyzky <104766555+Rezyzky@users.noreply.github.com>
2022-05-02 13:45:42 +0200

    Update Snake-Main.js

--- Snake-Main.js
+++ Snake-Main.js
@@ -1,4 +1,4 @@
-let felderProSeite, feldGroesse, kopfX, kopfY;
+let felderProSeite, feldGroesse, kopfX, kopfY, bewegungX, bewegungY;
 
 function setup() {
 	felderProSeite = 15;
@@ -6,13 +6,20 @@ function setup() {
 	createCanvas(felderProSeite * feldGroesse, felderProSeite * feldGroesse);
 	kopfX = 3 * feldGroesse;
 	kopfY = 4 * feldGroesse;
+    bewegungX = 0;
+    bewegungY = 0;
+    frameRate(4);
+    console.log('1. Ins Feld klicken');
+    console.log('2. Mit Pfeiltasten steuern');
+    console.log('3. Futter fressen');
 	
 }
 
 function draw() {
+    bewegeSchlange();
 	zeichneSpielfeld();
 	zeichneSchlange();
-	zeichneFeld();
+	
 }
 
 function zeichneSpielfeld() {
@@ -30,3 +37,29 @@ function zeichneFeld(x, y, farbe) {
 	fill(farbe);
 	rect(x, y, feldGroesse, feldGroesse);
 }
+function bewegeSchlange() {
+    kopfX = kopfX + bewegungX;
+    kopfY = kopfY + bewegungY;
+}
+
+function keyPressed() {
+    
+    if (key == "ArrowRight"){  
+        bewegungX = feldGroesse;
+        bewegungY = 0;
+    }
+    if (key == "ArrowLeft") { 
+        bewegungX = -feldGroesse;
+        bewegungY = 0;
+    }
+    if (key == "ArrowDown") {
+        bewegungX = 0 
+        bewegungY = feldGroesse;
+    }
+    if (key == "ArrowUp") {
+        bewegungX = 0 
+        bewegungY = -feldGroesse
+    }
+    return false;
+}
+    
