57fdcd855ac165d2fe4ea687f2a315c8fb59cd9f
dxniiis <76264314+dxniiis@users.noreply.github.com>
2022-05-02 13:20:53 +0200

    Create Snake-Main.js

--- Snake-Main.js
+++ Snake-Main.js
@@ -1,1 +1,20 @@
+//Ich und Luis machen es zusammen.
+
+let felderProSeite, feldGroesse;
+ 
+function setup () {
+    felderProSeite = 15;
+    feldGroesse = 18;
+    createCanvas(felderProSeite*feldGroesse, felderProSeite*feldGroesse);
+}
+ 
+function draw() {
+    zeichneSpielfeld();
+}
+
+function zeichneSpielfeld() {
+   stroke("Black");
+    fill("LawnGreen");
+    rect(0, 0, width, height); 
+}
 
