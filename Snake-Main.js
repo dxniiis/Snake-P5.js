78ca9f002ea413307931dee1ecffa4ba73fe8780
dxniiis <76264314+dxniiis@users.noreply.github.com>
2022-05-02 13:26:27 +0200

    Update Snake-Main.js

--- Snake-Main.js
+++ Snake-Main.js
@@ -1,22 +1,32 @@
-//Ich und Luis machen es zusammen.
+let felderProSeite, feldGroesse, kopfX, kopfY;
 
-
-
-let felderProSeite, feldGroesse;
- 
-function setup () {
-    felderProSeite = 15;
-    feldGroesse = 18;
-    createCanvas(felderProSeite*feldGroesse, felderProSeite*feldGroesse);
+function setup() {
+	felderProSeite = 15;
+	feldGroesse = 18;
+	createCanvas(felderProSeite * feldGroesse, felderProSeite * feldGroesse);
+	kopfX = 3 * feldGroesse;
+	kopfY = 4 * feldGroesse;
+	
 }
- 
+
 function draw() {
-    zeichneSpielfeld();
+	zeichneSpielfeld();
+	zeichneSchlange();
+	zeichneFeld();
 }
 
 function zeichneSpielfeld() {
-   stroke("Black");
-    fill("LawnGreen");
-    rect(0, 0, width, height); 
+	stroke("Black");
+	fill("LawnGreen");
+	rect(0, 0, width, height);
 }
 
+function zeichneSchlange() {
+	// Schlangenkopf
+	zeichneFeld(kopfX, kopfY, "DarkOrchid");
+}
+
+function zeichneFeld(x, y, farbe) {
+	fill(farbe);
+	rect(x, y, feldGroesse, feldGroesse);
+}
